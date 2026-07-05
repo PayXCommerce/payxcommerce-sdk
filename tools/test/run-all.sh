3558ed4d4fa431e4168d53387a6071cbd373b503
root <root@smtp1.payxcommerce.com>
2026-07-05 14:11:50 +0200

    Add XML validation to integration tests

--- tools/test/run-all.sh
+++ tools/test/run-all.sh
@@ -7,6 +7,9 @@ php "$ROOT_DIR/packages/php-sdk/tests/run.php"
 find "$ROOT_DIR/packages/php-sdk/src" -name '*.php' -print -exec php -l {} \;
 find "$ROOT_DIR/examples/raw-php" -name '*.php' -print -exec php -l {} \;
 find "$ROOT_DIR/plugins" -name '*.php' -print -exec php -l {} \;
+if command -v xmllint >/dev/null 2>&1; then
+  find "$ROOT_DIR/plugins" -name '*.xml' -print -exec xmllint --noout {} \;
+fi
 
 echo "All PayXCommerce integration checks passed."
 
